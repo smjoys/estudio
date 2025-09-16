f12bd778e1311e7e6b0247a4d4142c9c870f6938
smjoys <92125767+smjoys@users.noreply.github.com>
2025-09-16 18:51:39 +0000

    subir

---
+++ my-studio/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./css/bootstrap.min.css";
+import { Steam } from "./components/Steam";
+
+export function App() {
+  return (
+    <Steam />
+  );
+}
